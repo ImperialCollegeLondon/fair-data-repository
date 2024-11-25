b5cb3ec2ea8ccbafe1ac182f3c05a720d690fdbf
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2024-11-25 12:34:24 +0000

    Add logged in user as default creator in submission form

---
+++ site/ic_data_repo/config/utils.py
@@ -0,0 +1,38 @@
+"""Utilities for settings."""
+
+from typing import Any, List, Optional
+
+from flask_login import current_user
+
+
+def get_user() -> Optional[List[dict[str, Any]]]:
+    """Format the current user profile for the submission form.
+
+    The default user profile schema has two string properties;
+    full name and affiliations. More details (identifiers?) would
+    have to come from the SSO.
+    https://github.com/inveniosoftware/invenio-accounts/blob/master/invenio_accounts/profiles/schemas.py
+    """
+    affiliations = []
+
+    try:
+        name = current_user.user_profile["full_name"]
+        given_name = name.split(", ")[1]
+        family_name = name.split(", ")[0]
+    except KeyError:
+        return []
+
+    if "affiliations" in current_user.user_profile:
+        affiliations.append({"name": current_user.user_profile["affiliations"]})
+
+    return [
+        {
+            "person_or_org": {
+                "type": "personal",
+                "name": name,
+                "given_name": given_name,
+                "family_name": family_name,
+            },
+            "affiliations": affiliations,
+        },
+    ]

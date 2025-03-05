4cbc2233878652c0abd0ddfa8b3b7925f3e2f056
James Paul Turner <james.turner@imperial.ac.uk>
2025-03-05 15:43:12 +0000

    Test in a temp DB.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from invenio_app.factory import create_app
 
 
 @pytest.fixture(scope="module")
-def app():
+def app(db_uri):
     """Provide the Flask app object used by tests."""
     app = create_app()
 
@@ -13,6 +13,9 @@ def app():
     # invenio_app_rdm.config) contain "pool_timeout" which is not valid for use with
     # the test sqlite database
     app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {}
+
+    # Use a temporary database for testing.
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 
     with app.app_context():
         yield app

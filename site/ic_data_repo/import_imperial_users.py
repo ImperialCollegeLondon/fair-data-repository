b54456bbcc45d8b29fdc5e3e7026b59afdc48ea7
Alex Dewar <a.dewar@imperial.ac.uk>
2025-03-05 12:30:32 +0000

    Make separate script for importing ICL users

--- site/ic_data_repo/import_imperial_users.py
+++ site/ic_data_repo/import_imperial_users.py
@@ -268,29 +268,3 @@ def _add_names_to_invenio(users: list[ImperialUser]):
         input=names_str.encode(),
         check=True,
     )
-
-
-if __name__ == "__main__":
-    import os
-    import sys
-
-    client_id = os.getenv("ICL_OAUTH_CLIENT_ID")
-    client_secret = os.getenv("ICL_OAUTH_CLIENT_SECRET")
-    tenant_id = "2b897507-ee8c-4575-830b-4f8267c3d307"
-    if not client_id or not client_secret:
-        raise RuntimeError(
-            "ICL_OAUTH_CLIENT_ID and ICL_OAUTH_CLIENT secret env vars must be set"
-        )
-    client = get_client(tenant_id, client_id, client_secret)
-
-    # Make sure services are running
-    sp.run(
-        ["invenio-cli", "services", "start"],
-        check=True,
-        stdout=sp.DEVNULL,
-    )
-
-    # Let user specify max number of users to retrieve so they can things without
-    # loading the lot
-    max_count = int(sys.argv[1]) if len(sys.argv) > 1 else None
-    import_imperial_contributors_to_invenio(client, max_count)

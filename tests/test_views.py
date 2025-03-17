7a852b7a1a6770f7edeb551a295b7ec46acd9f8d
sahil raja <s.raja@imperial.ac.uk>
2025-03-17 17:06:44 +0000

    removing fixture

--- tests/test_views.py
+++ tests/test_views.py
@@ -22,37 +22,6 @@ def user_client(user, client):
     return user.login(client)
 
 
-@pytest.fixture(scope="module")
-def app_config(app_config):
-    """Update invenio app_config fixture."""
-    import json
-    import os
-
-    app_config["COLLECT_STORAGE"] = "flask_collect.storage.file"
-
-    instance_path = app_config.get("INSTANCE_PATH")
-    if not instance_path:
-        from flask import current_app
-
-        instance_path = getattr(
-            current_app,
-            "instance_path",
-            os.environ.get("INVENIO_INSTANCE_PATH", "/tmp"),
-        )
-
-    manifest_dir = os.path.join(instance_path, "static/dist")
-    manifest_path = os.path.join(manifest_dir, "manifest.json")
-
-    os.makedirs(manifest_dir, exist_ok=True)
-
-    with open(manifest_path, "w") as f:
-        json.dump({"entrypoints": {}, "chunks": {}}, f)
-
-    app_config["WEBPACKEXT_MANIFEST_PATH"] = manifest_path
-
-    return app_config
-
-
 def test_index_view(client):
     """Simple check that index view does not give an error when rendered."""
     res = client.get("/")

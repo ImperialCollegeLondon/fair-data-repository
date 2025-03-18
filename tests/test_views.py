1aa6c68e21cc89e47798087b4dfcc0cdb15670f7
sahil raja <s.raja@imperial.ac.uk>
2025-03-18 10:38:55 +0000

    pre commit changes

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,9 +1,9 @@
 """Placeholder module for tests of view functions."""
 
+import os
 import re
 
 import pytest
-import os
 
 
 @pytest.fixture
@@ -25,12 +25,13 @@ def user_client(user, client):
 
 def test_index_view(client):
     """Simple check that index view does not give an error when rendered."""
-    if os.environ.get('CI'):
+    if os.environ.get("CI"):
         pytest.skip("Skipping in CI environment due to missing webpack assets")
-    
+
     res = client.get("/")
     assert res.status_code == 200
     assert b"Imperial College London" in res.data
+
 
 def test_index_auth(user_client, app):
     """Check the index view with a logged in user."""

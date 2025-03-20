2c30c2ba67cb51e1b262f06cb7ba24f4a3d8afb2
James Paul Turner <james.turner@imperial.ac.uk>
2025-03-20 12:58:07 +0000

    Add and config OpenSearch and Redis containers for tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,39 @@ import pytest
 from invenio_app.factory import create_app as app_factory
 
 
+@pytest.fixture(scope="session")
+def opensearch_container():
+    """Start an OpenSearch container."""
+    from testcontainers.opensearch import OpenSearchContainer
+
+    with OpenSearchContainer("opensearchproject/opensearch:2.18.0") as container:
+        host = container.get_container_host_ip()
+        port = container.get_exposed_port(9200)
+
+        # settings only for development. DO NOT use in production!
+        container.with_env("bootstrap.memory_lock", "true")
+        container.with_env("OPENSEARCH_JAVA_OPTS", "-Xms512m -Xmx512m")
+        container.with_env("DISABLE_INSTALL_DEMO_CONFIG", "true")
+        container.with_env("DISABLE_SECURITY_PLUGIN", "true")
+        container.with_env("discovery.type", "single-node")
+
+        yield {"host": host, "port": port}
+
+
+@pytest.fixture(scope="session")
+def redis_container():
+    """Start a Redis container."""
+    from testcontainers.redis import RedisContainer
+
+    with RedisContainer() as container:
+        host = container.get_container_host_ip()
+        port = container.get_exposed_port(6379)
+
+        yield {"host": host, "port": port}
+
+
 @pytest.fixture(scope="module")
-def app_config(app_config):
+def app_config(opensearch_container, redis_container, app_config):
     """Update invenio app_config fixture."""
     from ic_data_repo.config import settings
 
@@ -16,6 +47,24 @@ def app_config(app_config):
     # invenio_app_rdm.config) contain "pool_timeout" which is not valid for use with
     # the test sqlite database
     app_config["SQLALCHEMY_ENGINE_OPTIONS"] = ""
+
+    # OpenSearch config.
+    opensearch_host = opensearch_container["host"]
+    opensearch_port = opensearch_container["port"]
+    app_config["SEARCH_HOSTS"] = [{"host": opensearch_host, "port": opensearch_port}]
+
+    # Redis config.
+    redis_host = redis_container["host"]
+    redis_port = redis_container["port"]
+    redis_url = f"redis://{redis_host}:{redis_port}"
+    app_config["CACHE_TYPE"] = "redis"
+    app_config["CACHE_REDIS_URL"] = f"{redis_url}/0"
+    app_config["IIIF_CACHE_REDIS_URL"] = f"{redis_url}/0"
+    app_config["ACCOUNTS_SESSION_REDIS_URL"] = f"{redis_url}/1"
+    app_config["CELERY_RESULT_BACKEND"] = f"{redis_url}/2"
+    app_config["RATELIMIT_STORAGE_URL"] = f"{redis_url}/3"
+    app_config["COMMUNITIES_IDENTITIES_CACHE_REDIS_URL"] = f"{redis_url}/4"
+
     return settings.__dict__ | app_config
 
 

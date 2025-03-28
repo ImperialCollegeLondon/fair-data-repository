791293c88cfc161568425e554d794272e0e397bd
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-03-28 17:00:47 +0000

    Add usage info to import_imperial_awards.py

--- app_data/import_imperial_awards.py
+++ app_data/import_imperial_awards.py
@@ -6,5 +6,9 @@ from pathlib import Path
 from ic_data_repo.vocabs import import_imperial_awards_to_invenio
 
 if __name__ == "__main__":
-    filename = Path(sys.argv[1])
+    try:
+        filename = Path(sys.argv[1])
+    except IndexError:
+        print("Usage: python import_imperial_awards.py <filename>")
+        sys.exit(1)
     import_imperial_awards_to_invenio(filename)

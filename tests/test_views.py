fde775e593f0586b2db8062d84af421b442e954c
James Paul Turner <james.turner@imperial.ac.uk>
2025-04-09 11:36:51 +0100

    Use API instead of service.

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,7 @@
 """Tests for the views."""
 
 import re
-from datetime import datetime
+from datetime import date
 from unittest.mock import patch
 
 import pytest
@@ -50,10 +50,20 @@ def test_index_auth(user_client, app):
     assert not re.search(r"/uploads/new(?!\?community=icl)", res.data.decode("utf-8"))
 
 
-def test_create_record_via_service(app, db, location, vocabularies):
+def get_csrf_token(client):
+    """Get the CSRF token from the client cookie jar."""
+    for cookie in client.cookie_jar:
+        if cookie.name == "csrftoken":
+            return cookie.value
+    raise ValueError("CSRF token not found in cookies")
+
+
+def test_metadata_schema(user_client, location, vocabularies):
     """Test the record metadata schema."""
-    from invenio_access.permissions import system_identity
-    from invenio_rdm_records.proxies import current_rdm_records_service
+    headers = {
+        "Content-Type": "application/json",
+        "X-CSRFToken": get_csrf_token(user_client),
+    }
 
     raw_data = {
         "metadata": {
@@ -73,12 +83,16 @@ def test_create_record_via_service(app, db, location, vocabularies):
         },
     }
 
-    result = current_rdm_records_service.create(system_identity, raw_data)
-    today = datetime.now().strftime("%Y-%m-%d")
+    result = user_client.post(
+        "/api/records",
+        json=raw_data,
+        headers=headers,
+    )
 
-    assert result.data["metadata"]["title"] == "Test Record"
-    assert result.data["metadata"]["resource_type"]["id"] == "dataset"
-    assert result.data["metadata"]["creators"][0]["person_or_org"]["name"] == "Neo"
-    assert "role" not in result.data["metadata"]["creators"][0]
-    assert result.data["metadata"]["publisher"] == "Imperial College London"
-    assert result.data["metadata"]["publication_date"] == today
+    assert result.status_code == 201
+    assert result.json["metadata"]["title"] == "Test Record"
+    assert result.json["metadata"]["resource_type"]["id"] == "dataset"
+    assert result.json["metadata"]["creators"][0]["person_or_org"]["name"] == "Neo"
+    assert "role" not in result.json["metadata"]["creators"][0]
+    assert result.json["metadata"]["publisher"] == "Imperial College London"
+    assert result.json["metadata"]["publication_date"] == date.today().isoformat()

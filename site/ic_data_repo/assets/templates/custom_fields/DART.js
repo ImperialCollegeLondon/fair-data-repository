caee9c4374fc79c5878f845f79d30a8cdb7a9269
sahil raja <s.raja@imperial.ac.uk>
2025-04-10 15:39:54 +0100

    adjusting js file

--- site/ic_data_repo/assets/templates/custom_fields/DART.js
+++ site/ic_data_repo/assets/templates/custom_fields/DART.js
@@ -1,61 +1,72 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Input } from "react-invenio-forms";
-import { Form } from "semantic-ui-react";
+import React, { Component } from "react";
 
-export function DartIdField(props) {
-  const {
-    fieldPath,
-    label,
-    icon,
-    placeholder,
-    clearable,
-  } = props;
+import { Input, Array } from "react-invenio-forms";
+import { Grid, Form, Button, Icon } from "semantic-ui-react";
 
-  return (
-    <Form.Field className="dart-id-field">
-      <Input
-        fieldPath={fieldPath}
-        label={label}
-        placeholder={placeholder}
-        icon={icon}
-        clearable={clearable}
-      />
+const newDataset = {
+  DartId: "",
+};
 
-      <div className="help-text" style={{ marginTop: '8px', color: '#666', fontSize: '0.9em' }}>
-        <p>
-          The DART ID is a unique identifier assigned to datasets in the Imperial College London
-          Data Assessment Repository Tool. This ID must be provided for all research data deposits.
-        </p>
-        <p>
-          <a
-            href="https://www.imperial.ac.uk/admin-services/secretariat/policies-and-guidance/data-assessments/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            More info
-          </a>
-        </p>
-      </div>
-    </Form.Field>
-  );
+export class Datasets extends Component {
+  render() {
+    const {
+      fieldPath, // injected by the custom field loader via the field config property
+      DartId,
+      icon,
+      addButtonLabel,
+      description,
+      label,
+    } = this.props;
+
+return (
+  <Array
+  fieldPath={fieldPath}
+  DartId={DartId}
+  label={label}
+  icon={icon}
+  addButtonLabel={addButtonLabel}
+  defaultNewValue={{ DartId: "", description: "" }} // include description here if desired
+  description={description}
+>
+  {({ arrayHelpers, indexPath }) => {
+    const fieldPathPrefix = `${fieldPath}.${indexPath}`;
+    return (
+      <Grid style={{ border: "white 3px solid", padding: "1.5rem", backgroundColor: "#eee", margin: "1rem" }}>
+        <Grid.Row>
+          <Grid.Column width="15">
+            <Input
+              fieldPath={`${fieldPathPrefix}.DartId`}
+              label="DartId"
+              placeholder="Enter DartId"
+            />
+          </Grid.Column>
+          <Grid.Column width="1">
+            <Form.Field style={{ marginTop: "1.75rem", float: "right" }}>
+              <Button
+                aria-label="Remove field"
+                className="close-btn"
+                icon
+                onClick={() => arrayHelpers.remove(indexPath)}
+                type="button"
+              >
+                <Icon name="close" />
+              </Button>
+            </Form.Field>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width="16">
+            <p>Please enter your DART id. For info about DART
+              <a href="https://www.imperial.ac.uk/admin-services/secretariat/policies-and-guidance/data-assessments/" target="_blank" rel="noopener noreferrer">
+                read more
+              </a>
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }}
+</Array>
+);
+  }
 }
-
-DartIdField.propTypes = {
-  fieldPath: PropTypes.string.isRequired,
-  label: PropTypes.string,
-  icon: PropTypes.string,
-  placeholder: PropTypes.string,
-  search: PropTypes.bool,
-  multiple: PropTypes.bool,
-  clearable: PropTypes.bool,
-};
-
-DartIdField.defaultProps = {
-  label: "DART ID",
-  icon: "address card outline",
-  placeholder: "DART ID",
-  search: false,
-  multiple: false,
-  clearable: true,
-};

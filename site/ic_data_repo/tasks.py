a990bddf779378269ef57d44b65d6f0f2f23e81a
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-05-27 13:26:19 +0100

    Minor refactor of tasks.py

--- site/ic_data_repo/tasks.py
+++ site/ic_data_repo/tasks.py
@@ -5,24 +5,20 @@ In particular, this includes tasks to be run periodically in the background.
 
 from celery import shared_task
 from flask import current_app
-from ic_data_repo.config import (
-    ICL_MICROSOFT_TENANT_ID,
-    ICL_OAUTH_CLIENT_ID,
-    ICL_OAUTH_CLIENT_SECRET,
-)
-from ic_data_repo.microsoft_graph_api_client import get_client
-from ic_data_repo.vocabs import import_imperial_contributors_to_invenio
+
+from .microsoft_graph_api_client import get_client
+from .vocabs import import_imperial_contributors_to_invenio
 
 
 @shared_task
 def update_imperial_users() -> None:
     """Update the list of possible contributors from Imperial."""
-    if not ICL_OAUTH_CLIENT_ID or not ICL_OAUTH_CLIENT_SECRET:
-        raise RuntimeError(
-            "ICL_OAUTH_CLIENT_ID and ICL_OAUTH_CLIENT_SECRET env vars must be set"
-        )
+    if not current_app.config["ICL_GRAPH_API_ENABLED"]:
+        return
 
     client = get_client(
-        ICL_MICROSOFT_TENANT_ID, ICL_OAUTH_CLIENT_ID, ICL_OAUTH_CLIENT_SECRET
+        current_app.config["ICL_MICROSOFT_TENANT_ID"],
+        current_app.config["ICL_OAUTH_CLIENT_ID"],
+        current_app.config["ICL_OAUTH_CLIENT_SECRET"],
     )
     import_imperial_contributors_to_invenio(client, current_app.logger)

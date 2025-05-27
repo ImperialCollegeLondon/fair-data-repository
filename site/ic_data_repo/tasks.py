b881ff1cbab538e51db8a2dba1b4ce15e232fce0
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2025-05-27 13:59:53 +0100

    Add celery task to sync record data to symplectic

--- site/ic_data_repo/tasks.py
+++ site/ic_data_repo/tasks.py
@@ -7,6 +7,7 @@ from celery import shared_task
 from flask import current_app
 
 from .microsoft_graph_api_client import get_client
+from .symplectic_interface import SymplecticClient
 from .vocabs import import_imperial_contributors_to_invenio
 
 
@@ -22,3 +23,16 @@ def update_imperial_users() -> None:
         current_app.config["ICL_OAUTH_CLIENT_SECRET"],
     )
     import_imperial_contributors_to_invenio(client, current_app.logger)
+
+
+@shared_task
+def export_record_to_symplectic(record) -> None:
+    """Syncronise record metadata to symplectic."""
+    if not current_app.config["SYMPLECTIC_ENABLED"]:
+        return
+
+    client = SymplecticClient(
+        current_app.config["SYMPLECTIC_API_URL"],
+        current_app.config["SYMPLECTIC_API_SUBSCRIPTION_KEY"],
+    )
+    client.create_record(record)

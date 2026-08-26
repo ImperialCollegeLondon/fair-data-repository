0770021292cbe82c93aa931056cab88fd521f75a
James Paul Turner <james.turner@imperial.ac.uk>
2026-08-26 11:40:42 +0100

    Add description transfer type and schema.

---
+++ site/ic_data_repo/description_transfer.py
@@ -0,0 +1,23 @@
+"""Local file transfer provider with optional description."""
+
+from invenio_records_resources.services.files.transfer.providers.local import (
+    LocalTransfer,
+)
+from marshmallow import fields, validate
+
+DESCRIPTION_TRANSFER_TYPE = "D"
+"""Identifier for description transfer type."""
+
+
+class DescriptionTransferSchema(LocalTransfer.Schema):
+    """Schema for description transfer type."""
+
+    description = fields.String(required=False, validate=validate.Length(max=100))
+
+
+class DescriptionTransfer(LocalTransfer):
+    """Local transfer with optional description."""
+
+    transfer_type = DESCRIPTION_TRANSFER_TYPE
+
+    Schema = DescriptionTransferSchema

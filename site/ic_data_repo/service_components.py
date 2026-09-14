19f9830f179e1bbfb43d924a7a29383b9411e9f2
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2026-09-14 12:59:39 +0100

    Apply changes from code review

--- site/ic_data_repo/service_components.py
+++ site/ic_data_repo/service_components.py
@@ -1,5 +1,6 @@
 """Module for custom service components."""
 
+from flask_principal import Identity
 from invenio_records_resources.services.records.components import ServiceComponent
 from invenio_records_resources.services.uow import TaskOp
 from invenio_vocabularies.proxies import current_service as vocabularies_service
@@ -19,9 +20,24 @@ class SymplecticComponent(ServiceComponent):
 class RestrictedLicensePermissionComponent(ServiceComponent):
     """Service component to enforce restricted license permissions."""
 
-    def _enforce_restricted_license_permission(self, identity, data):
+    def _enforce_restricted_license_permission(
+        self, identity: Identity, data: dict[str, object] | None
+    ):
         """Check if a restricted license is requested and test permission."""
-        deposit_licenses = data and data.get("metadata", {}).get("rights", [])
+        if not data:
+            return
+
+        # As the schema of data is not guaranteed when this component runs, we need to
+        # strictly check the types of the fields we access to avoid runtime errors. If
+        # it's not what we expect, just skip the permission check and let a following
+        # service component handle the validation and raise an error.
+        metadata = data.get("metadata")
+        if not isinstance(metadata, dict):
+            return
+        deposit_licenses = metadata.get("rights")
+        if not isinstance(deposit_licenses, list):
+            return
+
         vocab_entries = vocabularies_service.read_many(
             identity, type="licenses", ids=[dl["id"] for dl in deposit_licenses]
         )

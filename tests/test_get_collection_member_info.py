8ef0b7394d55816af4802839869fab40f1baa049
James Paul Turner <james.turner@imperial.ac.uk>
2026-09-23 11:59:37 +0100

    More tests.

--- tests/test_get_collection_member_info.py
+++ tests/test_get_collection_member_info.py
@@ -38,68 +38,158 @@ def record_data():
 
 
 def test_valid_related_records(db, search_clear, location, vocabularies, record_data):
-    """Test for correctness with a collection with valid related records."""
-    record_1_data = deepcopy(record_data)
-    record_1_data["metadata"]["title"] = "Test Record 1"
-    record_1_data["metadata"]["identifiers"] = [
+    """Test a collection with valid related records."""
+    r_1_data = deepcopy(record_data)
+    r_1_data["metadata"]["title"] = "Test Record 1"
+    r_1_data["metadata"]["identifiers"] = [
         {"identifier": "10.1234/collection1", "scheme": "doi"},
         {"identifier": "https://example.com/collection1", "scheme": "url"},
     ]
-    record_1_draft = records_service.create(system_identity, record_1_data)
-    record_1 = records_service.publish(system_identity, record_1_draft.id)
+    r_1_draft = records_service.create(system_identity, r_1_data)
+    r_1 = records_service.publish(system_identity, r_1_draft.id)
 
-    record_2_data = deepcopy(record_data)
-    record_2_data["metadata"]["title"] = "Test Record 2"
-    record_2_data["metadata"]["identifiers"] = [
+    r_2_data = deepcopy(record_data)
+    r_2_data["metadata"]["title"] = "Test Record 2"
+    r_2_data["metadata"]["identifiers"] = [
         {"identifier": "10.1234/collection2", "scheme": "doi"},
         {"identifier": "https://example.com/collection2", "scheme": "url"},
     ]
-    record_2_draft = records_service.create(system_identity, record_2_data)
-    record_2 = records_service.publish(system_identity, record_2_draft.id)
+    r_2_draft = records_service.create(system_identity, r_2_data)
+    r_2 = records_service.publish(system_identity, r_2_draft.id)
 
-    collection_data = deepcopy(record_data)
-    collection_data["metadata"]["related_identifiers"] = [
+    c_data = deepcopy(record_data)
+    c_data["metadata"]["related_identifiers"] = [
         {
-            "identifier": "10.1234/collection1",
+            "identifier": r_1.data["metadata"]["identifiers"][0]["identifier"],
             "relation_type": {"id": "haspart"},
             "scheme": "doi",
         },
         {
-            "identifier": "10.1234/collection2",
+            "identifier": r_2.data["metadata"]["identifiers"][0]["identifier"],
             "relation_type": {"id": "haspart"},
             "scheme": "doi",
         },
     ]
-    collection_draft = records_service.create(system_identity, collection_data)
-    collection = records_service.publish(system_identity, collection_draft.id)
+    c_draft = records_service.create(system_identity, c_data)
+    c = records_service.publish(system_identity, c_draft.id)
 
     search_clear.indices.refresh()
-    result = get_collection_member_info(system_identity, collection.id)
+    result = get_collection_member_info(system_identity, c.id)
     result.sort(key=lambda x: x["title"])
     assert len(result) == 2
     assert result[0] == {
-        "title": "Test Record 1",
-        "url": f"https://127.0.0.1:5000/records/{record_1.id}",
-        "dois": ["10.1234/collection1"],
+        "title": r_1.data["metadata"]["title"],
+        "url": r_1.data["links"]["self_html"],
+        "dois": [r_1.data["metadata"]["identifiers"][0]["identifier"]],
     }
     assert result[1] == {
-        "title": "Test Record 2",
-        "url": f"https://127.0.0.1:5000/records/{record_2.id}",
-        "dois": ["10.1234/collection2"],
+        "title": r_2.data["metadata"]["title"],
+        "url": r_2.data["links"]["self_html"],
+        "dois": [r_2.data["metadata"]["identifiers"][0]["identifier"]],
     }
 
 
-# def test_valid_related_draft_records():
-#     """Test for correctness with a collection with valid related draft records."""
+def test_valid_related_draft_records(
+    db, search_clear, location, vocabularies, record_data
+):
+    """Test a collection with valid related draft records."""
+    r_1_data = deepcopy(record_data)
+    r_1_data["metadata"]["identifiers"] = [
+        {"identifier": "10.1234/collection1", "scheme": "doi"},
+        {"identifier": "https://example.com/collection1", "scheme": "url"},
+    ]
+    r_1_draft = records_service.create(system_identity, r_1_data)
+
+    c_data = deepcopy(record_data)
+    c_data["metadata"]["related_identifiers"] = [
+        {
+            "identifier": r_1_draft.data["metadata"]["identifiers"][0]["identifier"],
+            "relation_type": {"id": "haspart"},
+            "scheme": "doi",
+        },
+    ]
+    c_draft = records_service.create(system_identity, c_data)
+    c = records_service.publish(system_identity, c_draft.id)
+
+    search_clear.indices.refresh()
+    result = get_collection_member_info(system_identity, c.id)
+    assert len(result) == 1
+    assert result[0] == {
+        "title": r_1_draft.data["metadata"]["title"],
+        "url": r_1_draft.data["links"]["self_html"],
+        "dois": [r_1_draft.data["metadata"]["identifiers"][0]["identifier"]],
+    }
 
 
-# Collection with no related records
+def test_nonlocal_related_records(
+    db, search_clear, location, vocabularies, record_data
+):
+    """Test a collection with no locally-hosted related records."""
+    c_data = deepcopy(record_data)
+    c_data["metadata"]["related_identifiers"] = [
+        {
+            "identifier": "10.1234/nonlocal",
+            "relation_type": {"id": "haspart"},
+            "scheme": "doi",
+        },
+    ]
+    c_draft = records_service.create(system_identity, c_data)
+    c = records_service.publish(system_identity, c_draft.id)
+
+    search_clear.indices.refresh()
+    result = get_collection_member_info(system_identity, c.id)
+    assert len(result) == 0
 
 
-# Collection with related records but none with relation_type=haspart
+def test_related_records_no_haspart(
+    db, search_clear, location, vocabularies, record_data
+):
+    """Test a collection with related records without relation_type=haspart."""
+    r_1_data = deepcopy(record_data)
+    r_1_data["metadata"]["identifiers"] = [
+        {"identifier": "10.1234/collection1", "scheme": "doi"},
+        {"identifier": "https://example.com/collection1", "scheme": "url"},
+    ]
+    r_1_draft = records_service.create(system_identity, r_1_data)
+    r_1 = records_service.publish(system_identity, r_1_draft.id)
+
+    c_data = deepcopy(record_data)
+    c_data["metadata"]["related_identifiers"] = [
+        {
+            "identifier": r_1.data["metadata"]["identifiers"][0]["identifier"],
+            "relation_type": {"id": "cites"},
+            "scheme": "doi",
+        },
+    ]
+    c_draft = records_service.create(system_identity, c_data)
+    c = records_service.publish(system_identity, c_draft.id)
+
+    search_clear.indices.refresh()
+    result = get_collection_member_info(system_identity, c.id)
+    assert len(result) == 0
 
 
-# Collection with related records but none with scheme=doi
+def test_related_records_no_doi(db, search_clear, location, vocabularies, record_data):
+    """Test a collection with related records without scheme=doi."""
+    r_1_data = deepcopy(record_data)
+    r_1_data["metadata"]["identifiers"] = [
+        {"identifier": "10.1234/collection1", "scheme": "doi"},
+        {"identifier": "https://example.com/collection1", "scheme": "url"},
+    ]
+    r_1_draft = records_service.create(system_identity, r_1_data)
+    r_1 = records_service.publish(system_identity, r_1_draft.id)
 
+    c_data = deepcopy(record_data)
+    c_data["metadata"]["related_identifiers"] = [
+        {
+            "identifier": r_1.data["metadata"]["identifiers"][1]["identifier"],
+            "relation_type": {"id": "haspart"},
+            "scheme": "url",
+        },
+    ]
+    c_draft = records_service.create(system_identity, c_data)
+    c = records_service.publish(system_identity, c_draft.id)
 
-# Wrong identity (no permission to read the collection)
+    search_clear.indices.refresh()
+    result = get_collection_member_info(system_identity, c.id)
+    assert len(result) == 0
